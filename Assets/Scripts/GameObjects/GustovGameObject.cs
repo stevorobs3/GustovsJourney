5ccec1cd6a39c4224d0e99d00ba1612e53383dc6
Steve <steve.robinson290391@gmail.com>
2017-05-13 23:32:38 +0100

    Fix Gustov Colliders
    Add Rigidbody2D and PolygonCollider2D to Gustov
    Add BoxCollider2D to the platforms
    Add PhysicsMaterial2D for Gustov + platforms

--- Assets/Scripts/GameObjects/GustovGameObject.cs
+++ Assets/Scripts/GameObjects/GustovGameObject.cs
@@ -6,12 +6,12 @@ namespace OnsightGames.Gustov.GameObjects
     {
         public void Awake()
         {
-            _rigidBody = GetComponent<Rigidbody>();
+            _rigidBody = GetComponent<Rigidbody2D>();
+            _collider = GetComponent<PolygonCollider2D>();
             _spriteRenderer = GetComponent<SpriteRenderer>();
-
         }
 
-        public void ApplyForce(Vector3 force, Vector2 maxVelocity)
+        public void ApplyForce(Vector2 force, Vector2 maxVelocity)
         {
             Debug.Log("Apply force " + force);
             _rigidBody.AddForce(force);
@@ -23,13 +23,18 @@ namespace OnsightGames.Gustov.GameObjects
         {
             var newX = MinAbs(maxVelocity.x, _rigidBody.velocity.x);
             var newY = MinAbs(maxVelocity.y, _rigidBody.velocity.y);
-            _rigidBody.velocity = new Vector3(newX, newY, _rigidBody.velocity.z);
+            _rigidBody.velocity = new Vector3(newX, newY);
         }
 
         private void PointSpriteInDirectionOfTravel()
         {
             if (Mathf.Abs(_rigidBody.velocity.x) > 0.00001f)
-                _spriteRenderer.flipX = _rigidBody.velocity.x > 0f;
+            {
+                var original = _spriteRenderer.transform.localScale;
+                var newX = _rigidBody.velocity.x > 0f ? -1f : 1f;
+                _spriteRenderer.transform.localScale = new Vector3(newX, original.y, original.z); 
+            }
+            
         }
 
         private static float MinAbs(float max, float original)
@@ -37,7 +42,8 @@ namespace OnsightGames.Gustov.GameObjects
             return Mathf.Min(Mathf.Abs(original), Mathf.Abs(max)) * Mathf.Sign(original);
         }
 
-        private Rigidbody _rigidBody;
+        private Rigidbody2D _rigidBody;
         private SpriteRenderer _spriteRenderer;
+        private PolygonCollider2D _collider;
     }
 }
