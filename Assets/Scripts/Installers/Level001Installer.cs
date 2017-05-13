37b5d1791c83fc46eca97a24a7b25341cde8a6a4
Steve <steve.robinson290391@gmail.com>
2017-05-13 15:10:37 +0100

    Add a blank scene with SceneContext
    This simply loads a PixelPerfectCamera

---
+++ Assets/Scripts/Installers/Level001Installer.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+public class Level001Installer : MonoInstaller {
+
+    public override void InstallBindings()
+    {
+        Container.Bind<PixelPerfectCamera>()
+            .FromComponentInNewPrefabResource("Prefabs/PixelPerfectCamera")
+            .AsSingle()
+            .NonLazy();
+    }
+}
